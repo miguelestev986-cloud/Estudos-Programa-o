5b6e5e86e1728ae2f371b31298fb66a9585b1897
Manguito <miguelestev986@gmail.com>
2025-10-20 15:57:04 -0300

    vault backup: 2025-10-20 15:57:04

---
+++ ex01.09 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int c, espaco_anterior;
+    espaco_anterior = 0; // indica se o caractere anterior foi um espaço
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (espaco_anterior == 0) {
+                putchar(c);
+                espaco_anterior = 1; // marca que o último caractere foi espaço
+            }
+        }
+        else {
+            putchar(c);
+            espaco_anterior = 0; // reseta quando não é espaço
+        }
+    }
+    return 0;
+}
