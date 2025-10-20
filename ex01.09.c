88a80bc7bc0e26923088cb0d1bb57f360e2a3e0d
Manguito <miguelestev986@gmail.com>
2025-10-20 15:17:34 -0300

    vault backup: 2025-10-20 15:17:34

---
+++ ex01.09.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*Escreva um programa que substitui
+uma sequência de espaços por apenas um espaço.*/
+
+int main(){
+
+    int ca;
+    while ((ca = getchar()) != EOF){
+        putchar(ca);
+        if (ca == " ")
+            printf("");
+    }
+    return 0;
+}
