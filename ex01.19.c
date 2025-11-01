ae06d55ef6b113f818ffea1cd1eb2202a19bf13b
Manguito <miguelestev986@gmail.com>
2025-11-01 20:00:56 -0300

    vault backup: 2025-11-01 20:00:56

---
+++ ex01.19.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAX 1000
+
+int tamanho(char f[]);
+void reverse(char f[]);
+
+int main(){
+    int c, i = 0;
+    char frase[MAX];
+
+    while((c = getchar()) != EOF){
+        if (c != '\n' && i < MAX-1)
+            frase[i++] = c;
+        else if(c == '\n'){
+            frase[i] = '\0';
+            reverse(frase);
+            printf("\n");
+            i = 0;
+        }
+    }
+    return 0;
+}
+
+int tamanho(char f[]){
+    int tamanho = 0;
+    while (f[tamanho] != '\0')
+        ++tamanho;
+    return tamanho;
+}
+
+void reverse(char f[]){
+    for(int t = tamanho(f)-1; t >= 0 ; --t)
+        putchar(f[t]);
+}
